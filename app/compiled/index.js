61dd6dae844b545eba194c55dcae5fe77f2c4944
Timothy Kempf <the.kaempfer@gmail.com>
2014-06-22 15:58:16 -0700

    setup basic gulp workflow for node-webkit apps

---
+++ app/compiled/index.js
@@ -0,0 +1,3 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof requireClient=="function"&&requireClient;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof requireClient=="function"&&requireClient;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(requireClient,module,exports){
+
+},{}]},{},[1])
---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+const LESS_ENTRY_FILE = './app/index.less';
+const JS_ENTRY_FILE = './app/index.js';
+const COMPILED_DIR = './app/compiled';
+
+var path = require('path');
+var vinyl_source = require('vinyl-source-stream');
+var watchify = require('watchify');
+var gulp = require('gulp');
+var gulp_less = require('gulp-less');
+var gulp_uglify = require('gulp-uglify');
+var gulp_minify_css = require('gulp-minify-css');
+var replace_stream = require('replace-stream');
+
+gulp.task( 'compile css', function(){
+	gulp.src( LESS_ENTRY_FILE )
+		.pipe( gulp_less({
+			paths: [ path.join( __dirname, 'app' ) ]
+		}))
+		.pipe( gulp.dest( COMPILED_DIR ) );
+});
+
+gulp.task( 'compile js', function(){
+	var w = watchify( JS_ENTRY_FILE );
+	w.transform('hbsfy');
+	//w.transform('browserify-shim');
+	var bundle = function(){
+		return w.bundle()
+			.pipe( replace_stream( 'require', 'requireClient' ) )
+			.pipe( replace_stream( 'nequire', 'require' ) )
+			.pipe( vinyl_source('index.js') )
+			.pipe( gulp.dest( COMPILED_DIR ) );
+	};
+	w.on( 'update', bundle );
+	w.on( 'error', function(){
+		console.log('error', arguments);
+	})
+	return bundle();
+});
+
+gulp.task( 'minify', function(){
+	gulp.src( COMPILED_DIR +'/**/*.js' )
+		.pipe( gulp_uglify() )
+		.pipe( gulp.dest( COMPILED_DIR ) );
+	gulp.src( COMPILED_DIR +'/**/*.css')
+		.pipe( gulp_minify_css({
+			keepSpecialComments: 0
+		}))
+		.pipe( gulp.dest( COMPILED_DIR ) );
+});
+
+gulp.task( 'watch css', function(){
+	gulp.watch( './app/index.less', ['compile css'] );
+});
+
+gulp.task( 'watch js', function(){
+	gulp.watch( './app/index.js', ['compile js'] );
+});
+
+gulp.task( 'default', ['compile css', 'compile js', 'watch css', 'watch js'] );
